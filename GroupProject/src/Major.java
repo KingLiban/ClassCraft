1d0136c9e600028ed796ed4cbbba233038a398b9
KingLiban <libanmohamed666@gmail.com>
2023-11-16 10:15:29 -0500

    test

--- GroupProject/src/Major.java
+++ GroupProject/src/Major.java
@@ -7,6 +7,7 @@ public class Major {
 	private ArrayList<String> requiredClasses;
 	private String studentEmail;
 	private int totalCredit;
+
 	public Major(int studYear, String studName, int iD, int credits, String email) {
 		studentYear = studYear;
 		studentName = studName;
@@ -17,39 +18,51 @@ public class Major {
 		Collections.addAll(requiredClasses, classes);
 		
 	}
+
 	public void addClasses(String c) {
 		requiredClasses.add(c);
 	}
+
 	public int getStudentYear() {
 		return studentYear;
 	}
+
 	public void setStudentYear(int studentYear) {
 		this.studentYear = studentYear;
 	}
+
 	public int getStudentID() {
 		return studentID;
 	}
+
 	public void setStudentID(int studentID) {
 		this.studentID = studentID;
 	}
+
 	public String getStudentName() {
 		return studentName;
 	}
+
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
+
 	public int getTotalCredit() {
 		return totalCredit;
 	}
+
 	public void setTotalCredit(int totalCredit) {
 		this.totalCredit = totalCredit;
 	}
+
 	public String getStudentEmail() {
 		return studentEmail;
 	}
+
 	public void setStudentEmail(String studentEmail) {
 		this.studentEmail = studentEmail;
 	}
+
 	@Override
 	public String toString() {
 		return "Student Name: " + this.studentName + 
