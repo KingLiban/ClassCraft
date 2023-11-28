3bfd421d3a99ab23f826b5ddad8eec2830656d29
Davud Azizov <azizovd@wit.edu>
2023-11-28 13:04:46 -0500

    It returns the selected and unselected classes

--- src/application/Scene2.java
+++ src/application/Scene2.java
@@ -29,59 +29,75 @@ import javafx.scene.layout.VBox;
 import java.util.*;
 
 public class Scene2 {
-	
+
     public static Scene createScene2(Stage stage, String major) {
-//    	stage.setTitle("Course Generator");
-		
-    	VBox layout = new VBox(10);
-    	layout.setAlignment(Pos.CENTER);
-		Text sceneTitle = new Text("Now, We ask that you select the classes you have completed");
-		sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));	
-		layout.getChildren().add(sceneTitle);
-//		String majors[] =
-//            { "Computer Science", "Information Technology", "Computer Networking",
-//                             "Data Science", "CyberSecurity", "Applied Mathematics" };
-//		ArrayList<String> classes = new ArrayList<>();
-		try{
-			File file;
-	        if (major.equals("Computer Science")) {
-	            file = new File("src/application/CompSci.txt");
-	        } else if (major.equals("Information Technology")) {
-	            file = new File("src/application/InformationTech.txt");
-	        } else if (major.equals("Computer Networking")) {
-	            file = new File("src/application/CompNetworking.txt");
-	        } else if (major.equals("Data Science")) {
-	            file = new File("src/application/DataScience.txt");
-	        } else if (major.equals("CyberSecurity")) {
-	            file = new File("src/application/CyberSecurity.txt");
-	        } else {
-	            file = new File("src/application/Math.txt");
-	        }
-	        
-			Scanner console = new Scanner(file);
-			while(console.hasNextLine()) {
-				String className = console.nextLine();
-//				classes.add(className);
-				CheckBox check_box = new CheckBox(className);
-				layout.getChildren().add(check_box);
-//				System.out.println(className);
-			}
-			
-			console.close();
-		} catch (FileNotFoundException e) {
-	        System.out.println("File not found: " + e.getMessage());
-	    } catch (Exception e) {
-	        System.out.println("Something went wrong: " + e.getMessage());
-	    }
 
-	    Button nextButton = new Button();
-	    nextButton.setText("Next");
+        VBox layout = new VBox(10);
+        layout.setAlignment(Pos.CENTER);
+        Text sceneTitle = new Text("Now, We ask that you select the classes you have completed");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        layout.getChildren().add(sceneTitle);
 
-	    layout.getChildren().add(nextButton);
+        ArrayList<String> selectedClasses = new ArrayList<>();
+        ArrayList<String> unselectedClasses = new ArrayList<>();
 
-	    Scene scene = new Scene(layout, 700, 680);
-	    return scene;
+        try {
+            File file;
+            if (major.equals("Computer Science")) {
+                file = new File("src/application/CompSci.txt");
+            } else if (major.equals("Information Technology")) {
+                file = new File("src/application/InformationTech.txt");
+            } else if (major.equals("Computer Networking")) {
+                file = new File("src/application/CompNetworking.txt");
+            } else if (major.equals("Data Science")) {
+                file = new File("src/application/DataScience.txt");
+            } else if (major.equals("CyberSecurity")) {
+                file = new File("src/application/CyberSecurity.txt");
+            } else {
+                file = new File("src/application/Math.txt");
+            }
+
+            Scanner console = new Scanner(file);
+
+            while (console.hasNextLine()) {
+                String className = console.nextLine();
+                CheckBox checkBox = new CheckBox(className);
+
+                checkBox.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                    if (newValue) {
+                        selectedClasses.add(className);
+                        unselectedClasses.remove(className); 
+                    } else {
+                        selectedClasses.remove(className);
+                        unselectedClasses.add(className);
+                    }
+                });
+
+                layout.getChildren().add(checkBox);
+                unselectedClasses.add(className); 
+            }
+
+            console.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+
+        Button nextButton = new Button();
+        nextButton.setText("Next");
+
+        nextButton.setOnAction(e -> {
+            System.out.println("Selected Classes: " + selectedClasses);
+            System.out.println("Unselected Classes: " + unselectedClasses);
+        });
+
+        layout.getChildren().add(nextButton);
+
+        Scene scene = new Scene(layout, 700, 680);
+        return scene;
     }
+}
 //    public static void checkBoxConstructor(String fileName) {
 //    	Scanner console = new Scanner(fileName);
 //		while(console.hasNextLine()) {
@@ -90,4 +106,4 @@ public class Scene2 {
 //		}
 //    }
     
-}
+
